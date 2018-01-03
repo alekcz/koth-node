f46a1bd2e82431c1a6920940e33277f3dfd16ed3
Alexander Oloo <alekcz@gmail.com>
2018-01-03 22:43:55 +0200

    initial commit

---
+++ core/layer.js
@@ -0,0 +1,57 @@
+'use strict'
+
+function createLayer (args) {
+	return {
+		"name":args.name || "layer",
+		"type":  args.type || "Layer",
+		"x": args.x || 100,
+	    "y": args.y || 100,
+	    "width": args.width || 250,
+	    "height": args.height || 250,
+	    "color": args.color || "#359DD9"
+	}
+}
+
+function ignoreKey(noflylist,key){
+	return noflylist.indexOf(key) >= 0
+}
+
+function renderChildren(object) {
+	if(object) {
+		if(object.hasOwnProperty("children")) {
+			var children = object.children;
+			for (var i = children.length - 1; i >= 0; i--) {
+				children[i]["parent"] = object.name
+				return renderSingle(children[i]);
+			}
+		}
+		else return "";
+	}
+	else return "";
+}
+
+function renderSingle(object) {
+	var string = "\n";
+	string = string + object.name + " = new " + object.type + "\n"
+	for(var key in object) {
+	  // We check if this key exists in the obj
+	  if (object.hasOwnProperty(key) && ( key != "name" || key != "type" )) {
+	    // someKey is only the KEY (string)! Use it to get the obj:
+	    if(!ignoreKey(["name","type","children"], key)) {
+	   		string = string + "\t"+key + ": " + object[key+""] + "\n";
+	    }
+	  }
+	}
+	return string + "" + renderChildren(object);
+}
+
+function render(object) {
+	var finalstring = "";
+	finalstring = renderSingle(object);
+	return finalstring;
+}
+
+module.exports =  {
+    createLayer: createLayer,
+    render: render
+};
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+const jsonfile = require('jsonfile');
+const core = require('./core/layer');
+const fs = require('fs-extra')
+
+
+function readFile(path,cb) {
+	
+	if(path)
+	{
+		jsonfile.readFile(path, function(err, obj) {
+		  cb(null, obj)
+		})
+	}
+
+}
+
+
+function createFramerViewfile (src,dest) {
+	readFile(src,function (err, data) {
+		if(err) return false;
+		//var file = core.createLayer(data);
+		var contents = core.render(data);
+		fs.outputFile(dest, contents, (err) => {  
+		    // throws an error, you could also catch it here
+		    if (err)
+		    {
+		    	console.log(err)
+		    	return false;
+		    }
+		    console.log(contents);
+		    return true;
+		});
+	});
+}
+
+module.exports =  {
+    createFramerViewfile: createFramerViewfile
+};
